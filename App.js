d0f46211822797b42d1df144a4af8850adf55f7b
Ijlal Furqaan <ijlalfurqaan5@gmail.com>
2023-03-10 10:52:30 +0530

    /

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import {createElement as ce} from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
 
-const heading = <h1>Namaste React</h1>
+const HeaderComponent  = () =>{
+    return  <h1>Functional Component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
